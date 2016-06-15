6e5c1b82ae7002231ebe0543f99b37239dc357db
mikko pohjalainen <mikko pohjalainen>
2016-06-15 22:04:52 +0300

    Organized javascript

--- ryapp/server/main.js
+++ ryapp/server/main.js
@@ -1,10 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
-
-
 Meteor.methods({
 	lisaaArkistoon: function(lisattava){
 		Arkisto.insert(lisattava);
